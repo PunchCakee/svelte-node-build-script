8a48266d62e3757567e8ecdf013b54d50d058cb3
Faisal Alghamdi <80383169+PunchCakee@users.noreply.github.com>
2023-03-10 04:55:57 +0300

    Add files via upload

---
+++ build-production.py
@@ -0,0 +1,28 @@
+import shutil
+import subprocess
+from contextlib import contextmanager
+import os
+
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+
+
+subprocess.run(["npm","run","build"], shell=True,stdout=True)
+shutil.copy2("package.json",'build')
+shutil.copy2("package-lock.json",'build')
+
+
+
+with cd('build/'):
+    subprocess.run(["npm","ci","--omit dev"], shell=True,stdout=True)
+
+
+
